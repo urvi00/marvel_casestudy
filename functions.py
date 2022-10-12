07ed9cbe20eeda8f1487fd6bf607c199900f8f4d
urvi gupta <urviguptareads@gmail.com>
2022-10-12 09:28:07 +0000

    added docstrings to functions

--- functions.py
+++ functions.py
@@ -3,6 +3,10 @@ import requests
 
 # function to create characters dataframe by making API call
 def create_character_df (input_apikey, input_hash, nameStartWith):
+    ''' 
+    inputs = public api key, hash, and starting character of name
+    outputs = pandas dataframe containing details of characters starting with the specified character.
+    '''
     ts = 1
     address = 'http://gateway.marvel.com/v1/public/characters'
     df = pd.DataFrame()
@@ -28,6 +32,14 @@ def create_character_df (input_apikey, input_hash, nameStartWith):
 
 # function to filter the characters dataframe
 def filter_characters(df, column_name, filter_condition, filter_value):
+    '''
+    inputs = characters dataframe, name of column that will be used for filtering,
+        filtering condition (for string value columns, filtering condition defaults to STARTS_WITH
+                            for integer value columns, filtering condition can be EQUAL_TO, GREATER_THAN, and LESS_THAN)
+        value of filter (value that the filter condition will be checked with)
+
+    outputs = filtered dataframe of characters
+    '''
     if column_name == 'name':
         filter_condition = 'starts_with'
         length =len(filter_value)
